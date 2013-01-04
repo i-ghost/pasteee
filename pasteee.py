300e3afa7671f99d63675e64ca01358c7bc90dee
i-ghost <i-ghost@wiki.tf>
2013-01-04 20:26:14 +0000

    pep8
    reformatted code for pep8 compliance

--- pasteee.py
+++ pasteee.py
@@ -7,70 +7,82 @@ Allows pasting to http://paste.ee
 http://github.com/i-ghost/pasteee
 """
 
-import urllib, urllib2, json
+import urllib
+import urllib2
+import json
+
 
 class PasteError(Exception):
-	"""Exception class for this module"""
-	pass
+    """Exception class for this module"""
+    pass
+
 
 class Paste(object):
-	"""A paste.ee dictionary object
-	
-	Returns a dictionary containing the following on a successful paste:
-	{
-		"id":"foobar",
-		"link":"http://paste.ee/p/foobar",
-		"raw":"http://paste.ee/r/foobar",
-		"download":"http://paste.ee/d/foobar"
-	}
-	
-	Raises a PasteError on an unsuccessful paste.
-	
-	Options:
-	----
-	paste - str, paste data to send
-	private - bool, indicates if paste should be private or public. Default: True
-	lang - str, indicates the syntax highlighting
-	key - str, API key. Default: "public"
-	desc - str, paste description. Default: ""
-	expire - int, expiration time in minutes; OR str, expiration in 'n views'
-	
-	http://paste.ee/wiki/API:Basics
-	----
-	
-	Doctests:
-	>>> from pasteee import Paste
-	>>> paste = Paste(u"Foo bar\\nBaz")
-	>>> print paste.keys()
-	[u'download', u'raw', u'link', u'id']
-	
-	Exception doctest:
-	>>> paste = Paste(u"Foo bar\\nBaz", lang=123456789)
-	Traceback (most recent call last):
-		File "<stdin>", line 1, in ?
-	PasteError: Invalid paste option: invalid_language
-	"""
-	def __new__(cls, paste, private=True, lang=u"plain", key=u"public", desc=u"", expire=0):
-		if not paste:
-			raise PasteError(u"No paste provided")
-		request = urllib2.Request("http://paste.ee/api",
-						data=urllib.urlencode({
-						'paste' : paste,
-						'private' : bool(private),
-						'language' : lang,
-						'key' : key,
-						'description' : desc,
-						'expire': expire
-						}))
-		try:
-			result = json.loads(urllib2.urlopen(request).read())
-			return result["paste"]
-		except urllib2.HTTPError:
-			print(u"Couldn't send paste")
-			raise
-		except KeyError:
-			raise PasteError(u"Invalid paste option: %s" %(result["error"]))
-			
+    """A paste.ee dictionary object
+
+    Returns a dictionary containing the following on a successful paste:
+    {
+        "id":"foobar",
+        "link":"http://paste.ee/p/foobar",
+        "raw":"http://paste.ee/r/foobar",
+        "download":"http://paste.ee/d/foobar"
+    }
+
+    Raises a PasteError on an unsuccessful paste.
+
+    Options:
+    ----
+    paste - str, paste data to send
+    private - bool, indicates if paste should be private or public. \
+    Default: True
+    lang - str, indicates the syntax highlighting
+    key - str, API key. Default: "public"
+    desc - str, paste description. Default: ""
+    expire - int, expiration time in minutes; OR str, expiration in 'n views'
+
+    http://paste.ee/wiki/API:Basics
+    ----
+
+    Doctests:
+    >>> from pasteee import Paste
+    >>> paste = Paste(u"Foo bar\\nBaz")
+    >>> print paste.keys()
+    [u'download', u'raw', u'link', u'id']
+
+    Exception doctest:
+    >>> paste = Paste(u"Foo bar\\nBaz", lang=123456789)
+    Traceback (most recent call last):
+        File "<stdin>", line 1, in ?
+    PasteError: Invalid paste option: invalid_language
+    """
+    def __new__(cls, paste,
+                private=True, lang=u"plain",
+                key=u"public", desc=u"",
+                expire=0):
+        if not paste:
+            raise PasteError(u"No paste provided")
+        request = urllib2.Request(
+            "http://paste.ee/api",
+            data=urllib.urlencode(
+                {
+                    'paste': paste,
+                    'private': bool(private),
+                    'language': lang,
+                    'key': key,
+                    'description': desc,
+                    'expire': expire
+                }
+            )
+        )
+        try:
+            result = json.loads(urllib2.urlopen(request).read())
+            return result["paste"]
+        except urllib2.HTTPError:
+            print(u"Couldn't send paste")
+            raise
+        except KeyError:
+            raise PasteError(u"Invalid paste option: %s" % (result["error"]))
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
